cf3c77864507369262197507785f24232a79daf2
prime <tonylrdzap@gmail.com>
2019-08-20 16:41:03 +0100

    few dev changes

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const websocket = require('ws')
+const websocket = require('ws');
 const response = require("./resources/responses.js")
 const device_handler = require('./device-handler.js');
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 const SILENT = true;
+
 
 express()
     .get("/api/:data", (req, res)=>{
         switch(req.params.data){
             case "election_data":
-                res.status(200).json(response.election_data)
-                break
+                res.status(200).json(response.election_data);
+                break;
             default:
-                res.status(404)
-                res.end("invalid")
+                res.status(404);
+                res.end("invalid");
         }
         SILENT && console.log(req.params.data, "fetched");
     })
     .listen(PORT, ()=>console.log(`server listening on port ${PORT}`));
-
