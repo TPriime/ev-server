7ebf25c2ccc2a9d6eefe4ed2dcc4bddd2b115b9c
prime <tonylrdzap@gmail.com>
2019-08-20 15:46:01 +0100

    added device handler

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-const response = require("./resources/responses.js")
 const express = require('express');
+const websocket = require('ws')
+const response = require("./resources/responses.js")
+const device_handler = require('./device-handler.js');
 
 const PORT = process.env.PORT || 8080
+const SILENT = true;
 
 express()
     .get("/api/:data", (req, res)=>{
@@ -13,5 +16,7 @@ express()
                 res.status(404)
                 res.end("invalid")
         }
+        SILENT && console.log(req.params.data, "fetched");
     })
     .listen(PORT, ()=>console.log(`server listening on port ${PORT}`));
+
